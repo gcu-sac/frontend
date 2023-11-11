4b50bceb533e9d0446b2e51dfd3a486411f3ebba
DongkwanKim00 <dkkim_it@naver.com>
2023-11-11 17:18:32 +0900

    modify links2

--- app/links.tsx
+++ app/links.tsx
@@ -1,3 +1,3 @@
-export const login_link = "https://sac.prod.cluster.yanychoi.com/user/login"
-export const signUp_link = "https://sac.prod.cluster.yanychoi.com/user/signup"
-export const calendar_event_link = "https://sac.prod.cluster.yanychoi.com/calendar/event"
+export const login_link = "https://sac.prod.cluster.yanychoi.com/api/auth/user/login"
+export const signUp_link = "https://sac.prod.cluster.yanychoi.com/api/auth/user/signup"
+export const calendar_event_link = "https://sac.prod.cluster.yanychoi.com/api/calendar/event"
